a27d561a966e5838f3cba0289e87e596acb3eb5c
cianfy <66866258+cianfy@users.noreply.github.com>
2022-04-20 15:48:39 +0200

    Aggiunto schermo I2C 16x02

--- main.py
+++ main.py
@@ -1,3 +1,109 @@
+def on_button_pressed_a():
+    for index in range(10):
+        basic.show_icon(IconNames.HEART)
+        basic.show_icon(IconNames.SMALL_HEART)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def RegistraNumero(numero_uscito: number):
+    global uno, due, tre, quattro, cinque, sei
+    if numero_uscito == 1:
+        uno += 1
+    elif numero_uscito == 2:
+        due += 1
+    elif numero_uscito == 3:
+        tre += 1
+    elif numero_uscito == 4:
+        quattro += 1
+    elif numero_uscito == 5:
+        cinque += 1
+    else:
+        sei += 1
+
+def on_gesture_shake():
+    global numero_uscito2
+    makerbit.clear_lcd1602()
+    makerbit.show_string_on_lcd1602("Sto lanciando",
+        makerbit.position1602(LcdPosition1602.POS1),
+        16,
+        TextOption.ALIGN_CENTER)
+    makerbit.show_string_on_lcd1602("il dado!",
+        makerbit.position1602(LcdPosition1602.POS17),
+        16,
+        TextOption.ALIGN_CENTER)
+    numero_uscito2 = LanciaDado()
+    MostraFaccia(numero_uscito2)
+    music.start_melody(music.built_in_melody(Melodies.BA_DING),
+        MelodyOptions.ONCE_IN_BACKGROUND)
+    RegistraNumero(numero_uscito2)
+    makerbit.clear_lcd1602()
+    makerbit.show_string_on_lcd1602("E' uscito",
+        makerbit.position1602(LcdPosition1602.POS1),
+        16,
+        TextOption.ALIGN_CENTER)
+    makerbit.show_string_on_lcd1602("il " + str(numero_uscito2) + " !!!",
+        makerbit.position1602(LcdPosition1602.POS17),
+        16,
+        TextOption.ALIGN_CENTER)
+    basic.pause(2000)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_button_pressed_b():
+    makerbit.clear_lcd1602()
+    if numero_uscito2 == 0:
+        makerbit.show_string_on_lcd1602("Non hai ancora",
+            makerbit.position1602(LcdPosition1602.POS1),
+            16,
+            TextOption.ALIGN_CENTER)
+        makerbit.show_string_on_lcd1602("lanciato il dado",
+            makerbit.position1602(LcdPosition1602.POS17),
+            16,
+            TextOption.ALIGN_CENTER)
+    else:
+        makerbit.show_string_on_lcd1602("L'ultimo numero",
+            makerbit.position1602(LcdPosition1602.POS1),
+            16,
+            TextOption.ALIGN_CENTER)
+        makerbit.show_string_on_lcd1602("uscito è il " + str(numero_uscito2),
+            makerbit.position1602(LcdPosition1602.POS17),
+            16,
+            TextOption.ALIGN_CENTER)
+    basic.pause(2000)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def LanciaDado():
+    for index2 in range(8):
+        MostraFaccia(randint(1, 6))
+        basic.pause(50)
+    return randint(1, 6)
+def MostraFaccia(num: number):
+    if num == 1:
+        faccia1.show_image(0)
+    elif num == 2:
+        faccia2.show_image(0)
+    elif num == 3:
+        faccia3.show_image(0)
+    elif num == 4:
+        faccia4.show_image(0)
+    elif num == 5:
+        faccia5.show_image(0)
+    else:
+        faccia6.show_image(0)
+numero_uscito2 = 0
+faccia6: Image = None
+faccia5: Image = None
+faccia4: Image = None
+faccia3: Image = None
+faccia2: Image = None
+faccia1: Image = None
+makerbit.connect_lcd(39)
+makerbit.show_string_on_lcd1602("Lancia dado",
+    makerbit.position1602(LcdPosition1602.POS1),
+    16,
+    TextOption.ALIGN_CENTER)
+makerbit.show_string_on_lcd1602("CLASSE 2D",
+    makerbit.position1602(LcdPosition1602.POS17),
+    16,
+    TextOption.ALIGN_CENTER)
 faccia1 = images.create_image("""
     . . . . .
         . . . . .
@@ -5,14 +111,58 @@ faccia1 = images.create_image("""
         . . . . .
         . . . . .
 """)
-faccia1 = images.create_image("""
+faccia2 = images.create_image("""
     . . . . .
+        . # . . .
         . . . . .
-        . . # . .
-        . . . . .
+        . . . # .
         . . . . .
 """)
+faccia3 = images.create_image("""
+    . . . . .
+        . # . . .
+        . . # . .
+        . . . # .
+        . . . . .
+""")
+faccia4 = images.create_image("""
+    . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        . . . . .
+""")
+faccia5 = images.create_image("""
+    . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+""")
+faccia6 = images.create_image("""
+    . . . . .
+        . # . # .
+        . # . # .
+        . # . # .
+        . . . . .
+""")
+uno = 0
+due = 0
+tre = 0
+quattro = 0
+cinque = 0
+sei = 0
+numero_uscito2 = 0
+basic.pause(2000)
+makerbit.clear_lcd1602()
 
 def on_forever():
-    pass
+    makerbit.show_string_on_lcd1602("1=" + str(uno) + " 2=" + str(due) + " 3= " + str(tre),
+        makerbit.position1602(LcdPosition1602.POS1),
+        16,
+        TextOption.ALIGN_CENTER)
+    makerbit.show_string_on_lcd1602("4=" + str(quattro) + " 5=" + str(cinque) + " 6= " + str(sei),
+        makerbit.position1602(LcdPosition1602.POS17),
+        16,
+        TextOption.ALIGN_CENTER)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 10; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
+    }
+})
 function RegistraNumero (numero_uscito: number) {
     if (numero_uscito == 1) {
         uno += 1
@@ -14,16 +20,31 @@ function RegistraNumero (numero_uscito: number) {
     }
 }
 input.onGesture(Gesture.Shake, function () {
+    makerbit.clearLcd1602()
+    makerbit.showStringOnLcd1602("Sto lanciando", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
+    makerbit.showStringOnLcd1602("il dado!", makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
     numero_uscito = LanciaDado()
     MostraFaccia(numero_uscito)
     music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.OnceInBackground)
     RegistraNumero(numero_uscito)
+    makerbit.clearLcd1602()
+    makerbit.showStringOnLcd1602("E' uscito", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
+    makerbit.showStringOnLcd1602("il " + numero_uscito + " !!!", makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
+    basic.pause(2000)
 })
-input.onButtonPressed(Button.AB, function () {
-    basic.showString("1: " + uno + " 2: " + due + " 3: " + tre + " 4: " + quattro + " 5: " + cinque + " 6: " + sei)
+input.onButtonPressed(Button.B, function () {
+    makerbit.clearLcd1602()
+    if (numero_uscito == 0) {
+        makerbit.showStringOnLcd1602("Non hai ancora", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
+        makerbit.showStringOnLcd1602("lanciato il dado", makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
+    } else {
+        makerbit.showStringOnLcd1602("L'ultimo numero", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
+        makerbit.showStringOnLcd1602("uscito è il " + numero_uscito, makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
+    }
+    basic.pause(2000)
 })
 function LanciaDado () {
-    for (let index = 0; index < 16; index++) {
+    for (let index = 0; index < 8; index++) {
         MostraFaccia(randint(1, 6))
         basic.pause(50)
     }
@@ -45,18 +66,15 @@ function MostraFaccia (num: number) {
     }
 }
 let numero_uscito = 0
-let sei = 0
-let cinque = 0
-let quattro = 0
-let tre = 0
-let due = 0
-let uno = 0
 let faccia6: Image = null
 let faccia5: Image = null
 let faccia4: Image = null
 let faccia3: Image = null
 let faccia2: Image = null
 let faccia1: Image = null
+makerbit.connectLcd(39)
+makerbit.showStringOnLcd1602("Lancia dado", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
+makerbit.showStringOnLcd1602("CLASSE 2D", makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
 faccia1 = images.createImage(`
     . . . . .
     . . . . .
@@ -72,11 +90,11 @@ faccia2 = images.createImage(`
     . . . . .
     `)
 faccia3 = images.createImage(`
-    # . . . .
     . . . . .
+    . # . . .
     . . # . .
+    . . . # .
     . . . . .
-    . . . . #
     `)
 faccia4 = images.createImage(`
     . . . . .
@@ -86,25 +104,29 @@ faccia4 = images.createImage(`
     . . . . .
     `)
 faccia5 = images.createImage(`
-    # . . . #
     . . . . .
+    . # . # .
     . . # . .
+    . # . # .
     . . . . .
-    # . . . #
     `)
 faccia6 = images.createImage(`
-    . # . # .
     . . . . .
     . # . # .
-    . . . . .
     . # . # .
+    . # . # .
+    . . . . .
     `)
-uno = 0
-due = 0
-tre = 0
-quattro = 0
-cinque = 0
-sei = 0
+let uno = 0
+let due = 0
+let tre = 0
+let quattro = 0
+let cinque = 0
+let sei = 0
+numero_uscito = 0
+basic.pause(2000)
+makerbit.clearLcd1602()
 basic.forever(function () {
-	
+    makerbit.showStringOnLcd1602("1=" + uno + " 2=" + due + " 3= " + tre, makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
+    makerbit.showStringOnLcd1602("4=" + quattro + " 5=" + cinque + " 6= " + sei, makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
 })
