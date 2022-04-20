aef3a6481b8bb08d57c0f1aa60e26ad09e428858
cianfy <66866258+cianfy@users.noreply.github.com>
2022-04-20 17:25:19 +0200

    Fix vari sullo schermo. Aggiunte statistiche cliccando il logo

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+function CalcolaStatistiche () {
+    makerbit.clearLcd1602()
+    makerbit.showStringOnLcd1602("Hai lanciato il", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
+    makerbit.showStringOnLcd1602("dado " + (uno + (due + (tre + (quattro + (cinque + sei))))) + " volte", makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
+    basic.pause(3000)
+}
 function RegistraNumero (numero_uscito: number) {
     if (numero_uscito == 1) {
         uno += 1
@@ -13,30 +19,6 @@ function RegistraNumero (numero_uscito: number) {
         sei += 1
     }
 }
-input.onGesture(Gesture.Shake, function () {
-    makerbit.clearLcd1602()
-    makerbit.showStringOnLcd1602("Sto lanciando", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
-    makerbit.showStringOnLcd1602("il dado!", makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
-    numero_uscito = LanciaDado()
-    MostraFaccia(numero_uscito)
-    music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.OnceInBackground)
-    RegistraNumero(numero_uscito)
-    makerbit.clearLcd1602()
-    makerbit.showStringOnLcd1602("E' uscito", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
-    makerbit.showStringOnLcd1602("il " + numero_uscito + " !!!", makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
-    basic.pause(2000)
-})
-input.onButtonPressed(Button.B, function () {
-    makerbit.clearLcd1602()
-    if (numero_uscito == 0) {
-        makerbit.showStringOnLcd1602("Non hai ancora", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
-        makerbit.showStringOnLcd1602("lanciato il dado", makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
-    } else {
-        makerbit.showStringOnLcd1602("L'ultimo numero", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
-        makerbit.showStringOnLcd1602("uscito è il " + numero_uscito, makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
-    }
-    basic.pause(2000)
-})
 function LanciaDado () {
     for (let index = 0; index < 8; index++) {
         MostraFaccia(randint(1, 6))
@@ -59,7 +41,12 @@ function MostraFaccia (num: number) {
         faccia6.showImage(0)
     }
 }
-let numero_uscito = 0
+let sei = 0
+let cinque = 0
+let quattro = 0
+let tre = 0
+let due = 0
+let uno = 0
 let faccia6: Image = null
 let faccia5: Image = null
 let faccia4: Image = null
@@ -111,16 +98,43 @@ faccia6 = images.createImage(`
     . # . # .
     . . . . .
     `)
-let uno = 0
-let due = 0
-let tre = 0
-let quattro = 0
-let cinque = 0
-let sei = 0
-numero_uscito = 0
-basic.pause(2000)
+uno = 0
+due = 0
+tre = 0
+quattro = 0
+cinque = 0
+sei = 0
+let numero_uscito = 0
+basic.pause(3000)
 makerbit.clearLcd1602()
 basic.forever(function () {
-    makerbit.showStringOnLcd1602("1=" + uno + " 2=" + due + " 3= " + tre, makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
-    makerbit.showStringOnLcd1602("4=" + quattro + " 5=" + cinque + " 6= " + sei, makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
+    makerbit.showStringOnLcd1602("1=" + uno + " 2=" + due + " 3=" + tre, makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
+    makerbit.showStringOnLcd1602("4=" + quattro + " 5=" + cinque + " 6=" + sei, makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
+    if (input.buttonIsPressed(Button.A)) {
+        makerbit.clearLcd1602()
+        makerbit.showStringOnLcd1602("Sto lanciando", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
+        makerbit.showStringOnLcd1602("il dado!", makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
+        numero_uscito = LanciaDado()
+        MostraFaccia(numero_uscito)
+        music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.OnceInBackground)
+        RegistraNumero(numero_uscito)
+        makerbit.clearLcd1602()
+        makerbit.showStringOnLcd1602("E' uscito", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
+        makerbit.showStringOnLcd1602("il " + numero_uscito + " !!!", makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
+        basic.pause(3000)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        makerbit.clearLcd1602()
+        if (numero_uscito == 0) {
+            makerbit.showStringOnLcd1602("Non hai ancora", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
+            makerbit.showStringOnLcd1602("lanciato il dado", makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
+        } else {
+            makerbit.showStringOnLcd1602("L'ultimo numero", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignCenter)
+            makerbit.showStringOnLcd1602("uscito e' " + numero_uscito, makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignCenter)
+        }
+        basic.pause(3000)
+    }
+    if (input.logoIsPressed()) {
+        CalcolaStatistiche()
+    }
 })
